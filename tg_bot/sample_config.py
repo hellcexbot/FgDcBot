afa009fcdbe33bffc48514ff7f05d0c1a700b478
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-01-08 19:11:46 +0300

    v0.0.1

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -4,7 +4,6 @@ if not __name__.endswith("sample_config"):
           "\nBot bırakılıyor...", file=sys.stderr)
     quit(1)
 
-
 # Create a new config.py file in same dir and import, then extend this class.
 class Config(object):
     LOGGER = True
