2faee3a7efe6f1b47326a3f9b42e031f82d5df6f
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-01-09 02:59:57 +0300

    v0.1.0

--- tg_bot/modules/dc.py
+++ tg_bot/modules/dc.py
@@ -1,181 +1,76 @@
-import random
+import random, asyncio
 from time import sleep
 
+from tg_bot.modules.helper_funcs.doguluksoru import DOGRU_SR_TEXT
+from tg_bot.modules.helper_funcs.cesaretsoru import CESARET_SR_TEXT
 from tg_bot import dispatcher
 from telegram import Message, Update, Bot, User
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
 from telegram.ext import CommandHandler, CallbackQueryHandler
 from telegram.ext.dispatcher import run_async
 
+
 @run_async
-def tkm(bot, update):
-	msg = update.effective_message
-	user = update.effective_user
-	chat = update.effective_chat
+def dc(bot, update):
+    chat = update.effective_chat
+    user = update.effective_user
+    msg = update.effective_message  # type: Optional[Message]
+    #SORU1 = msg.reply_text(random.choice(SORULAR_TEXT1))
+    if chat.type == "private":
+        text = f"𝐌𝐞𝐫𝐡𝐚𝐛𝐚 [{user.first_name}](tg://user?id={user.id}) 𝐔̈𝐳𝐠𝐮̈𝐧𝐮̈𝐦 𝐎̈𝐳𝐞𝐥 𝐌𝐞𝐬𝐚𝐣𝐝𝐚 𝐎𝐲𝐮𝐧 𝐎𝐲𝐧𝐚𝐲𝐚𝐦𝐚𝐳𝐬𝐢𝐧 :("
 
-	if chat.type == "private":
-		text = f"𝐌𝐞𝐫𝐡𝐚𝐛𝐚 [{user.first_name}](tg://user?id={user.id}) 𝐔̈𝐳𝐠𝐮̈𝐧𝐮̈𝐦 𝐎̈𝐳𝐞𝐥 𝐌𝐞𝐬𝐚𝐣𝐝𝐚 𝐎𝐲𝐮𝐧 𝐎𝐲𝐧𝐚𝐲𝐚𝐦𝐚𝐳𝐬𝐢𝐧 :("
-
-		KEYBOARD_S = [[InlineKeyboardButton(text="🤖 Beni Gruba Ekle",
-                                            url="tg://resolve?domain=FgDc_Bot&startgroup=a")]]
-		KEYBOARD_S += [[InlineKeyboardButton(text="📊 Oylamaya Katılmak İçin Tıkla",
+        text += "\n\n**Yeni Özelliğimizi Sevmediyseniz Botu Geliştirmemiz İçin Alttaki Button dan Oylamaya Katıla Bilirsin!**"
+        KEYBOARD_S = [[InlineKeyboardButton(text="🤖 Beni Gruba Ekle",
+                                             url="tg://resolve?domain=FgDc_Bot&startgroup=a")]]
+        KEYBOARD_S += [[InlineKeyboardButton(text="📊 Oylamaya Katılmak İçin Tıkla",
                                               url="https://t.me/fireqanQBotlari/10")]]
-		MARKU_P = InlineKeyboardMarkup(KEYBOARD_S)
+
+        MARKU_P = InlineKeyboardMarkup(KEYBOARD_S)
         msg.reply_text(text,
-        				parse_mode=ParseMode.MARKDOWN,
-        				reply_markup=MARKU_P)
-	else:
-		TAS = "🥌 TAŞ"
-		MAKAS = "✂️ MAKAS"
-		KAGIT = "📃 KAĞIT"
+                       parse_mode=ParseMode.MARKDOWN,
+                       reply_markup=MARKU_P)
 
-		keyboards = [[InlineKeyboardButton(text=TAS,
-											callback_data="tas_btn")]]
+    else:
+        text = f"[{user.first_name}](tg://user?id={user.id}) 𝐒𝐨𝐫𝐦𝐚𝐦𝐢 𝐈̇𝐬𝐭𝐞𝐝𝐢𝐠̆𝐢𝐧 𝐒𝐨𝐫𝐮 𝐓𝐢𝐩𝐢𝐧𝐢 𝐒𝐞𝐜̧:"
+        dogruluk_text = "Doğruluk"
+        cesaret_text = "Cesaret"
+        msg.reply_text(text,
+                        parse_mode=ParseMode.MARKDOWN,
+                        reply_markup=InlineKeyboardMarkup([
+                                        [InlineKeyboardButton(text=dogruluk_text,
+                                                                message_id=msg.message_id,
+                                                                callback_data="1")],
+                                        [InlineKeyboardButton(text=cesaret_text,
+                                                                message_id=msg.message_id,
+                                                                callback_data="2")]]))
+def button(bot, update):
+    query = update.callback_query
+    msg_id = query.id
+    msg = update.effective_message
+    user = update.effective_user
+    chat = update.effective_chat
 
-		keyboards += [[InlineKeyboardButton(text=KAGIT,
-											callback_data="kagit_btn")]]
-
-		keyboards += [[InlineKeyboardButton(text=MAKAS,
-											callback_data="makas_btn")]]
-
-		markup  = InlineKeyboardMarkup(keyboards)
-
-		msg.reply_text("[{user.first_name}](tg://user?id={user.id}) Birini Seç:",
-						parse_mode=ParseMode.MARKDOWN,
-						reply_markup=markup)
+    query.answer()
 
 
-def tkm_button(bot, update):
-	query = update.callback_query
-	msg = update.effective_message
-	user = update.effective_user
-	chat = update.effective_chat
-	tkm = ["Taş", "Kağıt", "Makas"]
-	tkm_random = random.choice(tkm)
-	berabere = "🤝 **BERABERE**"
-	win = f"✅ **[{user.first_name}](tg://user?id={user.id}) KAZANDI** 🥳"
-	bot_win = "❌ **BOT KAZANDI**"
-
-	query.answer()
-
-	if query.data == "tas_btn":
-		bot.delete_message(chat_id=chat.id,
+    if query.data == "1":
+        bot.delete_message(chat_id=chat.id,
                            message_id=msg.message_id)
-
-		bot.send_message(chat_id=chat.id,
-                         text=f"[{user.first_name}](tg://user?id={user.id}) 𝐓𝐚𝐬̧ 𝐒𝐞𝐜̧𝐞𝐧𝐞𝐠̆𝐢𝐧𝐢 𝐒𝐞𝐜̧𝐭𝐢...",
+        bot.send_message(chat_id=update.effective_chat.id,
+                         text=f"[{user.first_name}](tg://user?id={user.id}) 𝐈̇𝐜̧𝐢𝐧 𝐃𝐨𝐠̆𝐫𝐮𝐥𝐮𝐤 𝐒𝐨𝐫𝐮𝐬𝐮 𝐆𝐞𝐭𝐢𝐫𝐢𝐥𝐢𝐲𝐨𝐫...",
                          parse_mode=ParseMode.MARKDOWN)
-		bot.send_message(chat_id=chat.id,
-						  text="**Bot Seçim Yapıyor...**",
-						  parse_mode=ParseMode.MARKDOWN)
-
-		if tkm_random == "Taş":
-			sleep(120)
-			bot.send_message(chat_id=chat.id,
-							 text=f"Bot Seçim Yaptı: {tkm_random}")
-			sleep(30)
-			bot.send_message(chat_id=chat.id,
-							 text=f"{berabere}",
-							 parse_mode=ParseMode.MARKDOWN)
-
-		elif tkm_random == "Kağıt":
-			sleep(120)
-			bot.send_message(chat_id=chat.id,
-							 text=f"Bot Seçim Yaptı: {tkm_random}")
-			sleep(30)
-			bot.send_message(chat_id=chat.id,
-							 text=f"{bot_win}",
-							 parse_mode=ParseMode.MARKDOWN)
-			
-
-		elif tkm_random == "Makas":
-			sleep(120)
-			bot.send_message(chat_id=chat.id,
-							 text=f"Bot Seçim Yaptı: {tkm_random}")
-			sleep(30)
-			bot.send_message(chat_id=chat.id,
-							 text=f"{win}",
-							 parse_mode=ParseMode.MARKDOWN)
-
-	elif query.data == "kagit_btn":
-		bot.delete_message(chat_id=chat.id,
+        sleep(1)
+        bot.send_message(chat_id=update.effective_chat.id, text=f"{random.choice(DOGRU_SR_TEXT)}")
+    if query.data == "2":
+        bot.delete_message(chat_id=chat.id,
                            message_id=msg.message_id)
-
-		bot.send_message(chat_id=chat.id,
-                         text=f"[{user.first_name}](tg://user?id={user.id}) 𝐊𝐚𝐠𝐢𝐭 𝐒𝐞𝐜̧𝐞𝐧𝐞𝐠̆𝐢𝐧𝐢 𝐒𝐞𝐜̧𝐭𝐢...",
+        bot.send_message(chat_id=update.effective_chat.id,
+                         text=f"[{user.first_name}](tg://user?id={user.id}) 𝐈̇𝐜̧𝐢𝐧 𝐂𝐞𝐬𝐚𝐫𝐞𝐭 𝐒𝐨𝐫𝐮𝐬𝐮 𝐆𝐞𝐭𝐢𝐫𝐢𝐥𝐢𝐲𝐨𝐫...",
                          parse_mode=ParseMode.MARKDOWN)
-		bot.send_message(chat_id=chat.id,
-						  text="**Bot Seçim Yapıyor...**",
-						  parse_mode=ParseMode.MARKDOWN)
+        sleep(1)
+        bot.send_message(chat_id=update.effective_chat.id, text=f"{random.choice(CESARET_SR_TEXT)}")
 
-		if tkm_random == "Taş":
-			sleep(120)
-			bot.send_message(chat_id=chat.id,
-							 text=f"Bot Seçim Yaptı: {tkm_random}")
-			sleep(30)
-			bot.send_message(chat_id=chat.id,
-							 text=f"{win}",
-							 parse_mode=ParseMode.MARKDOWN)
-
-		elif tkm_random == "Kağıt":
-			sleep(120)
-			bot.send_message(chat_id=chat.id,
-							 text=f"Bot Seçim Yaptı: {tkm_random}")
-			sleep(30)
-			bot.send_message(chat_id=chat.id,
-							 text=f"{berabere}",
-							 parse_mode=ParseMode.MARKDOWN)
-			
-
-		elif tkm_random == "Makas":
-			sleep(120)
-			bot.send_message(chat_id=chat.id,
-							 text=f"Bot Seçim Yaptı: {tkm_random}")
-			sleep(30)
-			bot.send_message(chat_id=chat.id,
-							 text=f"{bot_win}",
-							 parse_mode=ParseMode.MARKDOWN)
-	
-	elif query.data == "makas_btn":
-		bot.delete_message(chat_id=chat.id,
-                           message_id=msg.message_id)
-
-		bot.send_message(chat_id=chat.id,
-                         text=f"[{user.first_name}](tg://user?id={user.id}) 𝐌𝐚𝐤𝐚𝐬 𝐒𝐞𝐜̧𝐞𝐧𝐞𝐠̆𝐢𝐧𝐢 𝐒𝐞𝐜̧𝐭𝐢...",
-                         parse_mode=ParseMode.MARKDOWN)
-		bot.send_message(chat_id=chat.id,
-						  text="**Bot Seçim Yapıyor...**",
-						  parse_mode=ParseMode.MARKDOWN)
-
-		if tkm_random == "Taş":
-			sleep(120)
-			bot.send_message(chat_id=chat.id,
-							 text=f"Bot Seçim Yaptı: {tkm_random}")
-			sleep(30)
-			bot.send_message(chat_id=chat.id,
-							 text=f"{bot_win}",
-							 parse_mode=ParseMode.MARKDOWN)
-
-		elif tkm_random == "Kağıt":
-			sleep(120)
-			bot.send_message(chat_id=chat.id,
-							 text=f"Bot Seçim Yaptı: {tkm_random}")
-			sleep(30)
-			bot.send_message(chat_id=chat.id,
-							 text=f"{win}",
-							 parse_mode=ParseMode.MARKDOWN)
-			
-
-		elif tkm_random == "Makas":
-			sleep(120)
-			bot.send_message(chat_id=chat.id,
-							 text=f"Bot Seçim Yaptı: {tkm_random}")
-			sleep(30)
-			bot.send_message(chat_id=chat.id,
-							 text=f"{berabere}",
-							 parse_mode=ParseMode.MARKDOWN)
-
-dc_handler = CommandHandler("tkm", tkm)
-dc_dc_handler = CallbackQueryHandler(tkm_button)
+dc_handler = CommandHandler("dc", dc)
+dc_dc_handler = CallbackQueryHandler(button)
 dispatcher.add_handler(dc_handler)
 dispatcher.add_handler(dc_dc_handler)
