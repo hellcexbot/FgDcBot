13dc7aee85afb002500a6948f21810bea938f0bb
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-01-10 15:19:47 +0300

    Update dc.py

--- tg_bot/modules/dc.py
+++ tg_bot/modules/dc.py
@@ -33,7 +33,7 @@ def dc(bot, update):
                        reply_markup=MARKU_P)
 
     else:
-        oyuncu += chat.id, user.id
+        oyuncu.extend(chat.id, user.id)
         text = f"[{user.first_name}](tg://user?id={user.id}) 𝐒𝐨𝐫𝐦𝐚𝐦𝐢 𝐈̇𝐬𝐭𝐞𝐝𝐢𝐠̆𝐢𝐧 𝐒𝐨𝐫𝐮 𝐓𝐢𝐩𝐢𝐧𝐢 𝐒𝐞𝐜̧:"
         dogruluk_text = "Doğruluk"
         cesaret_text = "Cesaret"
