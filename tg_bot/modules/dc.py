aa10db2b2719f764844bbc9e51e2069bb8db475c
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-01-10 20:56:32 +0300

    Update dc.py

--- tg_bot/modules/dc.py
+++ tg_bot/modules/dc.py
@@ -13,7 +13,7 @@ from telegram.ext.dispatcher import run_async
 OYUNCU = []
 
 @run_async
-def dc(self, update, e):
+def dc(self, update):
     chat = update.effective_chat
     user = update.effective_user
     msg = update.effective_message  # type: Optional[Message]
@@ -33,7 +33,7 @@ def dc(self, update, e):
                        reply_markup=MARKU_P)
 
     else:
-        e.OYUNCU.append(user.id)
+        OYUNCU.append(user.id)
         text = f"[{user.first_name}](tg://user?id={user.id}) 𝐒𝐨𝐫𝐦𝐚𝐦𝐢 𝐈̇𝐬𝐭𝐞𝐝𝐢𝐠̆𝐢𝐧 𝐒𝐨𝐫𝐮 𝐓𝐢𝐩𝐢𝐧𝐢 𝐒𝐞𝐜̧:"
         dogruluk_text = "Doğruluk"
         cesaret_text = "Cesaret"
@@ -49,7 +49,7 @@ def dc(self, update, e):
     self.send_message(chat_id=KOMUT_CHAT_İD,
                          text=KOMUT_CHAT_İD_TEXT.format(user.first_name, user.id, user.id, chat.title, chat.id, "dc"),
                          parse_mode=ParseMode.MARKDOWN)
-def button(self, update, e):
+def button(self, update):
     query = update.callback_query
     msg_id = query.id
     msg = update.effective_message
@@ -60,7 +60,7 @@ def button(self, update, e):
 
 
     if query.data == "1":
-        if e.OYUNCU != user.id:
+        if OYUNCU != user.id:
             self.send_message(chat_id=chat.id,
                               text="ss")
             
