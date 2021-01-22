fde4fb17152bf197b97f0ef84d9c670dd03ae6d3
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-01-23 02:26:15 +0300

    Update yapımcı.py

--- tg_bot/modules/yapımcı.py
+++ tg_bot/modules/yapımcı.py
@@ -4,13 +4,7 @@ from typing import Optional, List
 
 from tg_bot import dispatcher
 from telegram import Message, Chat, Update, Bot, User
-from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.error import Unauthorized, BadRequest, TimedOut, NetworkError, ChatMigrated, TelegramError
-from telegram.ext import CommandHandler, Filters, MessageHandler, CallbackQueryHandler
-from telegram.ext.dispatcher import run_async, DispatcherHandlerStop
-from telegram.utils.helpers import escape_markdown
-from tg_bot.__main__ import KOMUT_CHAT_İD_TEXT, KOMUT_CHAT_İD
-
+from 
 @run_async
 def sahip(bot: Bot, update: Update):
     chat = update.effective_chat  # type: Optional[Chat]
@@ -23,21 +17,9 @@ def sahip(bot: Bot, update: Update):
     KEYBOARDS += [[InlineKeyboardButton(text="Duyuru Kanalım",
                                          url="t.me/AzeBots")]]
 
-    KEYBOARDS += [[InlineKeyboardButton(text="Sahibimin Blog Kanalı",
-                                        url="t.me/intiqams")]]
-    # ONLY send settings in PM
-    if chat.type != chat.PRIVATE:
-        text = "Sahibim, Sahibimin Kanalları Ve Güncelleme V.b. Duyuru Kanallıma Aşağıdaki Butonlardan Ulaşa Bilirsiniz:)"
-        msg.reply_text(text,
-                          reply_markup=InlineKeyboardMarkup(KEYBOARDS))
-    else:
-        text = "Sahibim, Sahibimin Kanalları Ve Güncelleme V.b. Duyuru Kanallıma Aşağıdaki Butonlardan Ulaşa Bilirsiniz:)"
-        msg.reply_text(text,
-                          reply_markup=InlineKeyboardMarkup(KEYBOARDS))
-
+    KEYBOARDS += [[InlineKeyboardB
     bot.send_message(chat_id=KOMUT_CHAT_İD,
                          text=KOMUT_CHAT_İD_TEXT.format(user.first_name, user.id, user.id, chat.title, chat.id, "sahip"),
                          parse_mode=ParseMode.MARKDOWN)
 
-sahip_handler = CommandHandler("sahip", sahip)
-dispatcher.add_handler(sahip_handler)
+sahip_handler = CommandHandl
