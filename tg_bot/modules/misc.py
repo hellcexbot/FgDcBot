2554d8adcf2ab8fff86190e83f9afa1aa4dfd50a
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-01-23 02:23:34 +0300

    Update misc.py

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -4,20 +4,13 @@ import random
 from datetime import datetime
 from typing import Optional, List
 
-import requests
-from telegram import Message, Chat, Update, Bot, MessageEntity
-from telegram import ParseMode
-from telegram.ext import CommandHandler, run_async, Filters
+import rt import CommandHandler, run_async, Filters
 from telegram.utils.helpers import escape_markdown, mention_html
 
 from tg_bot import dispatcher
 from tg_bot.__main__ import STATS
 
 @run_async
-def stats(bot: Bot, update: Update):
-    update.effective_message.reply_text("Mevcut istatistikler:\n\n" + "\n".join([mod.__stats__() for mod in STATS]))
-
-
-STATS_HANDLER = CommandHandler("stats", stats, filters=Filters.user(1340915968))
+def stats(bot: Bot,ats, filters=Filters.user(1340915968))
 
 dispatcher.add_handler(STATS_HANDLER)
