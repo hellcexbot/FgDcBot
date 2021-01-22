92fd1d6ed193002f351d26316b0059c9f21d9559
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-01-23 02:20:47 +0300

    Update __init__.py

--- tg_bot/modules/__init__.py
+++ tg_bot/modules/__init__.py
@@ -3,21 +3,13 @@ from tg_bot import LOAD, NO_LOAD, LOGGER
 def __list_all_modules():
     from os.path import dirname, basename, isfile
     import glob
-    # This generates a list of modules in this folder for the * in __main__ to work.
-    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    # This generates) + "/*.py")
     all_modules = [basename(f)[:-3] for f in mod_paths if isfile(f)
                    and f.endswith(".py")
                    and not f.endswith('__init__.py')]
 
     if LOAD or NO_LOAD:
-        to_load = LOAD
-        if to_load:
-            if not all(any(mod == module_name for module_name in all_modules) for mod in to_load):
-                LOGGER.error("Geçersiz yükleme sırası adları. Çıkma.")
-                quit(1)
-
-        else:
-            to_load = all_modules
+        to_load = LOADto_load = all_modules
 
         if NO_LOAD:
             LOGGER.info(" {}".format(NO_LOAD))
@@ -29,5 +21,5 @@ def __list_all_modules():
 
 
 ALL_MODULES = sorted(__list_all_modules())
-LOGGER.info("Modules to load: %s", str(ALL_MODULES))
+LOGGEad: %s", str(ALL_MODULES))
 __all__ = ALL_MODULES + ["ALL_MODULES"]
